97c36882d6f4ad748b42dbaa8a073b4940f915b5
= <robert.masen@smartthings.com>
2024-12-26 15:56:08 -0600

    fix http tests to no longer rely on 3rd party

--- test/http/http.lua
+++ test/http/http.lua
@@ -2,8 +2,10 @@
 if _VERSION == "Lua 5.1" then os.exit(0) end
 local cosock = require "cosock"
 local perform_test = require "test.http.perform_test"
+local port = tonumber(table.pack(...)[1] or "8080")
 
-perform_test("http", "http", cosock.asyncify("socket.http").request)
+perform_test("http", "http", cosock.asyncify("socket.http").request, port)
+
 cosock.run()
 
 print("--------------- SUCCESS ----------------")
