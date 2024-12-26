53e2a21b4fd505670c129a284e02acf9dddf2eaf
= <robert.masen@smartthings.com>
2024-12-26 15:56:08 -0600

    fix http tests to no longer rely on 3rd party

--- test/http/perform_test.lua
+++ test/http/perform_test.lua
@@ -1,13 +1,15 @@
 local cosock = require "cosock"
 local socket = require "cosock.socket"
 
-local function slow_request(id, scheme, request, requests_started, requests_finished)
+local function slow_request(id, scheme, request, requests_started, requests_finished, port)
   return function()
     print("id", id)
     table.insert(requests_started, tostring(id))
 
     local starttime = socket.gettime()
-    local body, status = request(string.format("%s://bin.azdle.net/delay/3", scheme))
+    local url = string.format("%s://127.0.0.1:%s/delay/3", scheme, port)
+    print("requesting from", url)
+    local body, status = request(url)
     local endtime = socket.gettime()
 
     print(string.format("request took %s seconds", endtime - starttime))
@@ -21,8 +23,8 @@ local function slow_request(id, scheme, request, requests_started, requests_fini
   end
 end
 
-return function (name, scheme, request)
-  print("start")
+return function (name, scheme, request, port)
+  print(string.format("start->%s", name), port)
   local requests_started = {}
   local requests_finished = {}
   -- check that after 1 second that 3 requests have started, but that 0 have finished
@@ -35,9 +37,9 @@ return function (name, scheme, request)
     assert(#requests_finished == 0, name.." some requests already finished")
   end,
   "checker")
-  cosock.spawn(slow_request(1, scheme, request, requests_started, requests_finished), name.."-slow1")
-  cosock.spawn(slow_request(2, scheme, request, requests_started, requests_finished), name.."-slow2")
-  cosock.spawn(slow_request(3, scheme, request, requests_started, requests_finished), name.."-slow3")
+  cosock.spawn(slow_request(1, scheme, request, requests_started, requests_finished, port), name.."-slow1")
+  cosock.spawn(slow_request(2, scheme, request, requests_started, requests_finished, port), name.."-slow2")
+  cosock.spawn(slow_request(3, scheme, request, requests_started, requests_finished, port), name.."-slow3")
   cosock.run()
   print(name.." requests finished", #requests_finished)
   assert(#requests_finished == 3, name..": not all requests (or too many) finished")
