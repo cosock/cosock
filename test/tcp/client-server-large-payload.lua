2d730e414c19a6ed9269abeab89d3dbe2490d835
azdle <azdle@azdle.net>
2025-01-15 18:47:00 -0600

    more debug

--- test/tcp/client-server-large-payload.lua
+++ test/tcp/client-server-large-payload.lua
@@ -51,7 +51,7 @@ end, 'blob server')
 
 for i, chunk in ipairs(chunks) do
   cosock.spawn(function()
-    socket.select({}, {}, i* 1)
+    socket.select({}, {}, i* 0.1)
     print("client connect", chunk)
     local client = assert(socket.tcp())
     -- connect to the server
